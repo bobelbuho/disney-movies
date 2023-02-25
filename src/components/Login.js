316639d4eb2cb10379df2b4ce8ea5acf482e4878
bobelbuho <bob.vaz@hotmail.fr>
2023-02-25 18:43:02 +0100

    over

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,25 @@
 import styled from 'styled-components';
 const Login = (props) => {
-    return <div>Login</div>
+    <Container> 
+        <Content>Content</Content>
+    </Container>
 };
 
+const Container = styled.section`
+overflow: hidden;
+display: flex;
+flex_direction: column;
+text-align: center;
+height: 100vh; 
+`;
+
+const Content = styled.div`
+margin-bottom: 100vh;
+width: 100%;
+position: relative; 
+min-height: 100vh;
+box-sizing: border-box;
+display: flex; 
+justify-content: 
+`;
 export default Login;
